fd3cd446de09d7f5ae36f09a1d18a4f4ae5d7ead
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-01 21:02:21 +0100

    setup a basic blog Post Model WIP

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    published = models.DateField(auto_now_add=True)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
