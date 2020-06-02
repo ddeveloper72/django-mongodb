85d213cc4b0f62cfdd46f0db625c2c7d4122274e
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-02 05:54:22 +0100

    added Author and Entry models

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,35 @@
 from django.db import models
-from taggit.managers import TaggableManager
+# from taggit.managers import TaggableManager
 
 # Create your models here.
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    published = models.DateField(auto_now_add=True)
-    tags = TaggableManager()
-    slug = models.SlugField(unique=True, max_length=100)
+    name = models.CharField(max_length=200)
+    tags = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
 
     def __str__(self):
         return self.title
