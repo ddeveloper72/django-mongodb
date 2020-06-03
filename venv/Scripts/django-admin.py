1eb95364dde7721f45f7fde6dc8a37565ebf8bd4
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-03 22:18:35 +0100

    renamed venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\dunca\onedrive\the code institute\my visual studio projects\django-mongodb\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
