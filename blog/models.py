220620c0b275409f987c3a2fabbe7466d6295e70
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-03 23:57:45 +0100

    added mata abstract to Author model

--- blog/models.py
+++ blog/models.py
@@ -30,6 +30,9 @@ class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return self.name
 
