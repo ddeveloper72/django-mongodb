62ead13d98c4c8df33040fa6851a3682756de6b1
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-05 21:37:30 +0100

    removed classes that were impeding the db for debugging

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     name = models.CharField(max_length=200)
     tags = models.TextField()
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
     def __str__(self):
         return self.name
@@ -23,8 +23,8 @@ class MetaData(models.Model):
 
 
 # Djongo will never register this model as an actual model. 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
 
 class Author(models.Model):
@@ -32,26 +32,26 @@ class Author(models.Model):
     email = models.EmailField()
 
     # Djongo will never register this model as an actual model.
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
     def __str__(self):
         return self.name
 
 
 class Entry(models.Model):
-    blog = models.EmbeddedField(
-        model_container=Post,
-    )
-    meta_data = models.EmbeddedField(
-        model_container=MetaData,
-    )
+    # blog = models.EmbeddedField(
+    #     model_container=Post,
+    # )
+    # meta_data = models.EmbeddedField(
+    #     model_container=MetaData,
+    # )
     title = models.CharField(max_length=255)
     body_text = models.TextField()
     
-    authors = models.ArrayField(
-        model_container=Author,
-    )
+    # authors = models.ArrayField(
+    #     model_container=Author,
+    # )
     n_comments = models.IntegerField()
  
     def __str__(self):
