20bec75fbb3e8d8c5c94608396f1bf7768efb8a8
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-09 00:31:20 +0100

    Created simple view to render Entries model

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.views import View
+from .models import Entry, Blog
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+def blogs(request):
+    # create a view that gets all the blog entries and
+    # renders them to the markup
+    entries = Entry.objects.all()
+
+    return render(request, 'blogs.html', {'entries': entries})
