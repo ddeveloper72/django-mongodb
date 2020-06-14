877c59d28238f6d9e55608f999be75017edc1212
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-14 16:51:13 +0100

    updated models added to Admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Entry
+from .models import Post
 
 # Register your models here.
 
 
-admin.site.register([Entry])
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
+
+# admin.site.register(Post)
