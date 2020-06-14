ec06a578a4ed0c531b5f405a22b4f26ad8a0cf53
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-14 16:50:33 +0100

    refsactored model completely removing embedded model

--- blog/models.py
+++ blog/models.py
@@ -1,118 +1,27 @@
 from djongo import models
-from django import forms
+from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=40)
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                               related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        abstract = True
-    
-    def __str__(self):
-        return self.name
-
-
-class Blog(models.Model):
-    name = models.CharField(max_length=40)
-    tagline = models.TextField()
-    pub_date = models.DateTimeField(blank=True, null=True,
-                                    default=timezone.now)
-
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        return self.name
-
-
-class BlogForm(forms.ModelForm):
-
-    class Meta:
-        model = Blog
-        fields = (
-            'name', 'tagline', 'pub_date'
-        )
-        widgets = {
-            'name': forms.Textarea(attrs={'placeholder':
-                                          'Blog Title'}),
-            'tagline': forms.Textarea(attrs={'placeholder':
-                                             'About this Blog'})
-        }
-
-
-class Comment(models.Model):
-    name = models.CharField(max_length=40)
-    comment_text = models.TextField(max_length=360)
-    pub_date = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        abstract = True
+        ordering = ['-created_on']
 
     def __str__(self):
-        return self.author
-
-
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = (
-            'name', 'comment_text'
-        )
-        widgets = {
-            'name': forms.Textarea(attrs={'placeholder':
-                                          'Add your name'}),
-            'comment_text': forms.Textarea(attrs={'placeholder':
-                                                  'Comment on this blog'})
-        }
-
-# metadata model representing dates and intergers such as pingbacks
-# and ratings
-
-
-class MetaData(models.Model):
-    pub_date = models.DateField()
-    mod_date = models.DateField()
-    n_pingbacks = models.IntegerField()
-    rating = models.IntegerField()
-
-
-# Djongo will never register this model as an actual model.
-
-    class Meta:
-        abstract = True
-
-
-class MetaDataForm(forms.ModelForm):
-
-    class Meta:
-        model = MetaData
-        fields = (
-            'pub_date', 'mod_date',
-            'n_pingbacks', 'rating'
-        )
-
-
-class Entry(models.Model):
-    blog = models.EmbeddedModelField(
-        model_container=Blog,
-        model_form_class=BlogForm
-    )
-    author = models.EmbeddedModelField(
-        model_container=Author)
-    meta_data = models.EmbeddedModelField(
-        model_container=MetaData,
-        model_form_class=MetaDataForm
-    )
-    headline = models.CharField(max_length=80)
-    body_text = models.TextField(max_length=360)
-    comment = models.EmbeddedModelField(
-        model_container=Comment,
-        model_form_class=CommentForm
-    )
-    n_comments = models.IntegerField()
-
-    def __str__(self):
-        return self.headline
+        return self.title
