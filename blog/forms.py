efe72c76639d43d71b84e567e4c145b9d3577121
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-15 21:25:29 +0100

    updated the form to work new model

--- blog/forms.py
+++ blog/forms.py
@@ -1,1 +1,32 @@
 from django import forms
+from .models import Post
+
+
+class BlogForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = (
+            'title', 'content', 'content'
+        )
+        widgets = {
+            'title': forms.Textarea(attrs={'placeholder':
+                                           'Post Title'}),
+            'content': forms.Textarea(attrs={'placeholder':
+                                             'About your post here...'})
+        }
+
+
+# class CommentForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Comment
+#         fields = (
+#             'name', 'comment_text'
+#         )
+#         widgets = {
+#             'name': forms.Textarea(attrs={'placeholder':
+#                                           'Add your name'}),
+#             'comment_text': forms.Textarea(attrs={'placeholder':
+#                                                   'Comment on this blog'})
+#         }
