3cd72cfad4aa362507ce762e0c031cbb53e0b184
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-17 17:32:14 +0100

    created a new add/edit post view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect, reverse
 from django.views import View
+from django.template.defaultfilters import slugify
 from .models import Post
 from .forms import BlogForm
 from django.utils import timezone
 from django.contrib import messages
 
 # Create your views here.
-
 
 def PostList(request):
     # create a view that gets all the blog entries and
@@ -27,6 +27,25 @@ def PostDetail(request, slug):
 
     except:
         return HttpResponse('No blog entries have been added')
+
+
+def create_a_post(request, slug=None):
+    # and a new blog to the collection
+
+    post = get_object_or_404(Post, slug=slug) if slug else None
+    form = BlogForm(request.POST, instance=post)
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.slug = slugify(post.title)
+            post = form.save()
+            return redirect(PostDetail, post.slug)
+        else:
+            form = BlogForm(instance=post)
+            return render(request, 'add_blog.html', {'form': form})
+    else:
+        form = BlogForm(instance=post)
+        return render(request, 'add_blog.html', {'form': form})
 
 
 def create_or_edit_a_post(request, slug=None):
