f5de101417b730bb5cbdcda307212285d451ddfa
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-27 13:35:07 +0100

    Updated admin to benefit working with embedded models

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('headline', 'status', 'author')
+    list_display = ('headline', 'status', 'author',)
     list_filter = ("status",)
-    search_fields = ['headline', 'content', 'author']
+    # FOREIGN KEY SEARCHES MUST REF RELATED FIELD IN FOREIGN DOCUMENT
+    search_fields = ['headline', 'content', 'author__username', ]
     prepopulated_fields = {'slug': ('headline',)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'approved_comment', 'created_on',)
+    list_filter = ("approved_comment",)
+    # FOREIGN KEY SEARCHES MUST REF RELATED FIELD IN FOREIGN DOCUMENT
+    search_fields = ['comment_text', 'post__headline', 'name__username', ]
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
