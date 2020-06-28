999480107bebe64e06f8c01620fb3666d442e696
ddeveloper72 <duncan.niamh@gmail.com>
2020-06-28 13:54:53 +0100

    updated static files location

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_LOCATION = 'static'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "blog", "static"),
-)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_LOCATION = 'media'
+
 MEDIA_URL = '/media/'
